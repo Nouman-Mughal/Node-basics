bb650bb2f4a9e722bf168815dbe84ea35f156f69
Noman-Mughal <programmer.noman786@gmail.com>
2022-04-11 11:05:04 +0000

    2nd commit

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-app.get("/", (req, res) => {
-  console.log("hello world");
-  res.status(200).sendFile(path.join(__dirname, "index.js/index.html"));
+app.get("/", async (req, res) => {
+  await res.status(200).sendFile(path.join(__dirname, "index.js/index.html"));
 });
-app.get("/about", (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, "index.js/about.html"));
+app.get("/about", async (req, res) => {
+  await res.status(200).sendFile(path.join(__dirname, "index.js/about.html"));
 });
-app.get("/contact-me", (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, "index.js/contact-me.html"));
+app.get("/contact-me", async (req, res) => {
+  await res
+    .status(200)
+    .sendFile(path.join(__dirname, "index.js/contact-me.html"));
 });
-app.get("*", (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "index.js/404.html"));
+app.get("*", async (req, res) => {
+  await res.status(404).sendFile(path.join(__dirname, "index.js/404.html"));
 });
 
 app.listen(8080, () => {
